d447d448100c5c9452797fd922a83dc0051140f4
Ivaylo Tsokov <ivaylo.tsokov@dxc.com>
2019-06-08 13:31:02 +0300

    fixing the test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-        return "Les1 Jackson1";
+        return "Les Jackson";
         }
 
         // POST api/values
